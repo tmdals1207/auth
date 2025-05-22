290b20d807d5f16b724cd08ce048ad0c003ba5e6
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-05-22 23:35:03 +0900

    Fix : CI 에러 수정 8 (Feat #7)

--- src/test/java/com/mysite/auth/AuthIntegrationTest.java
+++ src/test/java/com/mysite/auth/AuthIntegrationTest.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AuthIntegrationTest {
