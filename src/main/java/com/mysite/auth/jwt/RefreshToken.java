9e05d7fac7d9af4e3c34a25a23d032fba3cf6111
ghdtmdalsda <ghdtmdalsdad@naver.com>
2025-05-22 15:28:25 +0900

    Feat : RefreshToken 저장 및 AccessToken 재생성 로직 추가 (Feat #3)

--- src/main/java/com/mysite/auth/jwt/RefreshToken.java
+++ src/main/java/com/mysite/auth/jwt/RefreshToken.java
@@ -6,12 +6,15 @@ import lombok.*;
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 public class RefreshToken {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String email;  // 사용자 식별용
 
     @Column(nullable = false)
--- src/main/java/com/mysite/auth/jwt/RefreshTokenRepository.java
+++ src/main/java/com/mysite/auth/jwt/RefreshTokenRepository.java
@@ -2,5 +2,8 @@ package com.mysite.auth.jwt;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByEmail(String email);
 }
